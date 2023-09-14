6acd0d30ac9dd6583d29a9635cd81db650b03b08
vu.vu <vu.vu@prismtechinc.io>
2023-09-14 10:49:24 +0700

    move merchant and timeslots

--- prismvio/merchant/api/serializers.py
+++ prismvio/merchant/api/serializers.py
@@ -1,12 +1,69 @@
 from rest_framework import serializers
 from timezone_field.rest_framework import TimeZoneSerializerField
 
-from prismvio.merchant.models import Merchant
+from prismvio.merchant.models import ExclusionDate, Merchant, TimeslotCollectionMerchant
+from prismvio.menu_merchant.models import Category
+from prismvio.merchant.exceptions import MerchantAlreadyExistsException
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
 
 
 class MerchantSerializer(serializers.ModelSerializer):
     timezone = TimeZoneSerializerField(use_pytz=True)
+    categories = CategorySerializer(many=True, read_only=True)
+    category_ids = serializers.ListField(child=serializers.IntegerField(), required=False, allow_null=True,
+                                         write_only=True)
 
     class Meta:
         model = Merchant
         fields = "__all__"
+
+    def validate(self, attrs):
+        request = self.context.get("request")
+        user = request.user
+        if user.merchants.exists():
+            raise MerchantAlreadyExistsException()
+        return attrs
+
+    def create(self, validated_data):
+        category_ids = validated_data.pop('category_ids', [])
+        merchant = super().create(validated_data)
+        if category_ids:
+            merchant.categories.set(category_ids)
+        return merchant
+
+
+class MerchantPreviewSerializer(serializers.ModelSerializer):
+    timezone = TimeZoneSerializerField(read_only=True)
+    categories = CategorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Merchant
+        fields = ('id', 'name', 'description', 'timezone', 'email', 'phone_number', 'currency',
+                  'website', 'is_active', 'uid', 'latitude', 'longitude', 'location', 'categories',
+                  'created_at', 'updated_at','total_available_slot','total_available_slots_unit', 'is_staffs_visible')
+        read_only_fields = fields
+
+
+class ExclusionDateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExclusionDate
+        fields = "__all__"
+
+
+class TimeslotCollectionMerchantSerializer(serializers.ModelSerializer):
+    exclusion_dates = serializers.SerializerMethodField()
+
+    class Meta:
+        model = TimeslotCollectionMerchant
+        fields = ("id", "weekly", "daily", "exclusion_dates", "deleted_at", "created_at", "updated_at", "merchant")
+
+    def get_exclusion_dates(self, obj):
+        exclusion_dates = ExclusionDate.objects.filter(merchant=obj.merchant)
+        if exclusion_dates:
+            return ExclusionDateSerializer(exclusion_dates, many=True).data
+        return []
--- prismvio/merchant/api/views.py
+++ prismvio/merchant/api/views.py
@@ -1,40 +1,153 @@
-from rest_framework import status, viewsets
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from django.http import JsonResponse
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from prismvio.core.permissions import CanDeleteMerchant, IsBusinessAdminOrAdmin
-from prismvio.merchant.models import Merchant
+from prismvio.core.permissions import MerchantPermission
+from prismvio.merchant.api.serializers import ExclusionDateSerializer, TimeslotCollectionMerchantSerializer
+from prismvio.merchant.models import ExclusionDate, Merchant, TimeslotCollectionMerchant
+from prismvio.utils.drf_utils import search
 
-from .serializers import MerchantSerializer
+from .serializers import MerchantSerializer, MerchantPreviewSerializer
+from prismvio.merchant.exceptions import MerchantDoesNotExistsException
 
 
-class MerchantViewSet(viewsets.ModelViewSet):
-    queryset = Merchant.objects.all()
+class MerchantListCreateView(generics.ListCreateAPIView):
     serializer_class = MerchantSerializer
-    permission_classes = [IsAuthenticated, IsBusinessAdminOrAdmin]
+    permission_classes = [MerchantPermission]
+
+    def get_queryset(self):
+        queryset = search(
+            query_params=self.request.query_params,
+            model=Merchant,
+        )
+        return queryset
 
     def perform_create(self, serializer):
-        # Check if the user already has a merchant
-        user = self.request.user
-        if user.merchants.exists():
-            # Return a 400 Bad Request response if the user already has a merchant
-            return Response({"detail": "You already have a merchant."}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Continue with the default creation process if the user doesn't have a merchant
         serializer.save(owner=self.request.user)
 
-    def perform_update(self, serializer):
-        # Check if the user already has a merchant
-        user = self.request.user
-        if not user.merchants.exists():
-            return Response({"detail": "You do not have a merchant to update."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # update merchant
-        serializer.save()
+class MerchantDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = MerchantPreviewSerializer
+    permission_classes = [MerchantPermission]
 
-    @action(detail=True, methods=["DELETE"], permission_classes=[CanDeleteMerchant])
-    def delete_merchant(self, request, pk=None):
-        merchant = self.get_object()
-        merchant.delete()
-        return Response({"detail": "Merchant deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+    def get_object(self):
+        try:
+            return Merchant.objects.get(pk=self.kwargs.get('pk'))
+        except Merchant.DoesNotExist:
+            raise MerchantDoesNotExistsException()
+
+
+class TimeslotsAPIView(APIView):
+    permission_classes = [MerchantPermission]
+
+    def get(self, request, merchant_id):
+        try:
+            timeslot_collection = TimeslotCollectionMerchant.objects.get(merchant=merchant_id)
+            serializer = TimeslotCollectionMerchantSerializer(timeslot_collection)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except TimeslotCollectionMerchant.DoesNotExist:
+            return Response(
+                {"error": "Timeslot collection not found for the given merchant"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+    def post(self, request, merchant_id, *args, **kwargs):
+        data = request.data
+        weekly = data.get("weekly", [])
+        daily = data.get("daily", [])
+
+        merchant = Merchant.objects.get(pk=merchant_id)
+
+        # Check if timeslot collection already exists for the merchant
+        timeslot_collection, created = TimeslotCollectionMerchant.objects.get_or_create(merchant=merchant)
+
+        # Update the timeslot collection with the provided data
+        timeslot_collection.weekly = weekly
+        timeslot_collection.daily = daily
+        timeslot_collection.save()
+
+        # Serialize the response
+        serializer = TimeslotCollectionMerchantSerializer(timeslot_collection)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ShiffsAPIView(APIView):
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.request.method == "GET":
+            self.permission_classes = (AllowAny,)
+        return super().get_permissions()
+
+    def get(self, request, merchant_id):
+        try:
+            timeslot_collection = TimeslotCollectionMerchant.objects.get(merchant_id=merchant_id)
+            shifts = timeslot_collection.shifts
+            return JsonResponse({"shifts": shifts}, status=200)  # Return 200 OK with the shifts data
+        except TimeslotCollectionMerchant.DoesNotExist:
+            return JsonResponse({"error": "Merchant not found"}, status=404)
+
+    def post(self, request, merchant_id, *args, **kwargs):
+        data = request.data
+        shifts = data.get("shifts", [])
+
+        # Fetch the timeslot collection for the given merchant_id
+        timeslot_collection, created = TimeslotCollectionMerchant.objects.get_or_create(merchant_id=merchant_id)
+
+        # Update the shifts for the timeslot collection
+        timeslot_collection.shifts = shifts
+        timeslot_collection.save()
+
+        return JsonResponse({}, status=200)  # Return 200 OK with an empty response body
+
+
+class ExclusionDateAPIVIew(APIView):
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.request.method == "GET":
+            self.permission_classes = (AllowAny,)
+        return super().get_permissions()
+
+    def get(self, request, merchant_id):
+        exclusion_dates = ExclusionDate.objects.filter(merchant_id=merchant_id)
+        serializer = ExclusionDateSerializer(exclusion_dates, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, merchant_id, *args, **kwargs):
+        datetimes = request.data.get("datetimes", [])
+        data = []
+        for datetime in datetimes:
+            datetime["merchant"] = merchant_id
+            serializer = ExclusionDateSerializer(data=datetime)
+            if serializer.is_valid():
+                serializer.save()
+                data.append(serializer.data)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(data, status=status.HTTP_201_CREATED)
+
+    def put(self, request, merchant_id, *args, **kwargs):
+        datetimes = request.data.get("datetimes", [])
+        data = []
+        for datetime in datetimes:
+            try:
+                exclusion_date = ExclusionDate.objects.get(id=datetime.get("id"))
+                serializer = ExclusionDateSerializer(exclusion_date, data=datetime, partial=True)
+                if serializer.is_valid():
+                    serializer.save()
+                    data.append(serializer.data)
+                else:
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            except ExclusionDate.DoesNotExist:
+                return Response({"error": "Exclusion date not found"}, status=status.HTTP_404_NOT_FOUND)
+        return Response(data, status=status.HTTP_200_OK)
+
+    def delete(self, request, merchant_id):
+        ids_to_delete = request.data
+        ExclusionDate.objects.filter(id__in=ids_to_delete).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

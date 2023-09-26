0a57abf567183187e70865523d182329b8117c8e
vu.vu <vu.vu@prismtechinc.io>
2023-09-26 16:17:24 +0700

    update env and base file

--- config/settings/base.py
+++ config/settings/base.py
@@ -98,7 +98,7 @@ LOCAL_APPS = [
     "prismvio.bookings",
     "prismvio.notifications",
     # search should get end
-    # "prismvio.search",
+    "prismvio.search",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS

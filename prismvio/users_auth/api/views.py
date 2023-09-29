5ee1e629ccdc38982d9a7f90d87d7c25fb68efe5
vu.vu <vu.vu@prismtechinc.io>
2023-09-29 14:00:22 +0700

    revert signup

--- prismvio/users_auth/api/views.py
+++ prismvio/users_auth/api/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from rest_framework import exceptions, status
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView

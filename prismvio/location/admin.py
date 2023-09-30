fb25643e97b110f8de1eb2fa6052edd57fdeb9bc
baothi <baothi246@gmail.com>
2023-09-30 17:28:32 +0700

    fix long lat user

--- prismvio/location/admin.py
+++ prismvio/location/admin.py
@@ -5,19 +5,27 @@ from prismvio.location.models import Country, District, Province, Ward
 
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["code", "full_name_vi", "full_name_en"]
+    search_fields = ["code", "full_name_vi", "full_name_en"]
+    list_per_page = 20
 
 
 @admin.register(Province)
 class ProvinceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["code", "name_vi", "name_en", "country"]
+    search_fields = ["code", "name_vi", "name_en", "country"]
+    list_per_page = 20
 
 
 @admin.register(District)
 class DistrictAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["code", "name_vi", "name_en", "province"]
+    search_fields = ["code", "name_vi", "name_en", "province"]
+    list_per_page = 20
 
 
 @admin.register(Ward)
 class WardAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["code", "name_vi", "name_en", "district"]
+    search_fields = ["code", "name_vi", "name_en", "district"]
+    list_per_page = 20

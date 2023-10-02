7659255967e8040c38ce4e3335c987dc0d8794ba
vu.vu <vu.vu@prismtechinc.io>
2023-10-02 18:02:26 +0700

    add currency

--- prismvio/merchant/api/serializers.py
+++ prismvio/merchant/api/serializers.py
@@ -111,6 +111,7 @@ class MerchantSerializer(serializers.ModelSerializer):
             "category_ids",
             "categories",
             "keywords",
+            "currency",
         )
         read_only_fields = ("id", "created_at", "updated_at", "categories")
 
